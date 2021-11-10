5b6f4dc2a1b618e07fea8cbda1ed676a11a985a8
gIsaak <isacco.gobbi@gmail.com>
2021-11-10 12:50:56 +0100

    Clean

--- config.h
+++ config.h
@@ -3,8 +3,7 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-                                "monospace:size=8",
+static const char *fonts[] = {  "monospace:size=8",
                                 "Font Awesome 5 Free Regular:size=8",
                                 "Font Awesome 5 Free Solid:size=8" };
 
